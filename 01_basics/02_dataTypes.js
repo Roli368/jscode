8867199116bffcce9406a3c6d6245383c5500d42
Roli368 <rolirathour368@gmail.com>
2025-05-31 10:52:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3)   //we are using node.js not browser
+
+let name="roli"
+let age=45
+let isLoggedIn= true
+
+//number=>2to power 53
+//bigint  in tradding 
+// string=>""
+// bolean=>true/false
+//null=>standalone value
+// undefind=>
+//symbol unique
+// object
+
+console.log(typeof "roli");
+console.log(typeof null); // object
+// undefined ka undefined hi hai
