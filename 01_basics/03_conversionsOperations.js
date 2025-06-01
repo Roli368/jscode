f5d52fd26d7a8227fc515645b8b13db539360a54
Roli368 <rolirathour368@gmail.com>
2025-06-01 04:25:32 +0000

    conversionsOperations

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -1,25 +1,25 @@
 let score="33"
-console.log(typeof score);
-console.log(typeof (score)); // both return string //string
+//console.log(typeof score);
+//console.log(typeof (score)); // both return string //string
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber); //number
-console.log(valueInNumber); //33
+//console.log(typeof valueInNumber); //number
+//console.log(valueInNumber); //33
 
 
 let value ="yu89 "
-console.log(typeof value);
-console.log(typeof (value)); // both return string //string
+//console.log(typeof value);
+//console.log(typeof (value)); // both return string //string
 
 let valueInNum=Number(value)
-console.log(typeof valueInNum); //number
-console.log(valueInNum); //NaN     thats why typescript is used
+//console.log(typeof valueInNum); //number
+//console.log(valueInNum); //NaN     thats why typescript is used
 
 // NaN ka type number hai     
 
 let isLoggedIn="roli"
 let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=>true; 0=> false
 //""=> false
@@ -27,7 +27,33 @@ console.log(booleanIsLoggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+/// ********************* Operations***********************************
+let num=5
+let negNum=-num
+console.log(negNum);
+/* console.log(2+2)
+console.log(2-2)
+console.log(2**2)
+console.log(2%2)
+
+console.log(2+"1") //21
+console.log("2"+1) //21
+console.log(3+2+"4") //54
+console.log("6"+6+2) //662
+console.log(true) // true
+console.log(+true)//1  ye nhi krna chahia
+console.log(+"")// 0  ye nhi krna chahia */
+
+let gameCounter= 100
+++gameCounter;
+console.log(gameCounter)
+gameCounter++;                  // 101 in both cases but pre and post increament
+console.log(gameCounter)
+
+
+
 
 
