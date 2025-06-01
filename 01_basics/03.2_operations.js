0a4257c1b26cd0726f081e843284b23b6821e28f
Roli368 <rolirathour368@gmail.com>
2025-06-01 04:28:59 +0000

    operation.2

---
+++ 01_basics/03.2_operations.js
@@ -0,0 +1,22 @@
+let num=5
+let negNum=-num
+console.log(negNum);
+/* console.log(2+2)
+console.log(2-2)
+console.log(2**2)
+console.log(2%2)
+
+console.log(2+"1") //21
+console.log("2"+1) //21
+console.log(3+2+"4") //54
+console.log("6"+6+2) //662
+console.log(true) // true
+console.log(+true)//1  ye nhi krna chahia
+console.log(+"")// 0  ye nhi krna chahia */
+
+let gameCounter= 100
+++gameCounter;
+console.log(gameCounter)
+gameCounter++;                  // 101 in both cases but pre and post increament
+console.log(gameCounter)
+
