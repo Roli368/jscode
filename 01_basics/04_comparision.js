ee96a6af7f3e7d88721b1d7dd4078a3799116e6c
Roli368 <rolirathour368@gmail.com>
2025-06-01 08:38:19 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+//console.log(2>1)
+//console.log(3<2)
+
+console.log("2">1) //true
+console.log("02">1)  // true
+console.log(null>0)     //false
+console.log(null==0)        //false
+console.log(null>=0)        //true   ==,<=,>=,.. work differentlly comparision converts null to zero that why null>=0 is true and null>0 is false
+//equality and comparision diff
+// for each type of com undefined gives false
+// console.log("2"===2) false as it check both data type and value
