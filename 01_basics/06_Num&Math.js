8fff6a026fd0434b2e9c930590574124b6298f81
Roli368 <rolirathour368@gmail.com>
2025-06-02 03:58:26 +0000

    Number and Math

---
+++ 01_basics/06_Num&Math.js
@@ -0,0 +1,34 @@
+/*const score=400
+console.log(score);
+
+const balance =new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber=123.76821
+console.log(otherNumber.toFixed(2));
+console.log(otherNumber.toPrecision(1));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN')); */
+
+//########################## MATHS##################################
+
+//console.log(Math);                      // object
+//console.log(Math.ceil(6.78));
+//console.log(Math.floor(6.8));
+//console.log(Math.round(8.93));
+//console.log(Math.abs(-6));                // - gives absolute
+//console.log(Math.min(3,4,5,1,~9));        // ~ increased by one 
+console.log(Math.random());          //between 0to 1
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+
+
+const min=10
+const max=20
+console.log(Math.floor((Math.random()*10))+min);
+console.log(Math.floor((Math.random()*(max-min+1)))+min);    // formula for number in the range
