b16d026375f9335aef61924f4ff615cdfbfa5c56
Roli368 <rolirathour368@gmail.com>
2025-06-03 06:26:20 +0000

    condition_challanges

---
+++ 02_basics/condition_challanges.js
@@ -0,0 +1,47 @@
+// checking a number is greater than another number
+let num1=5
+let num2=8
+
+if (num1>num2){
+    console.log("num1 is greater than num2")
+}
+else{
+   console.log("Nope , num1 is smaller")
+}
+// to chek equal string 
+let userName= "roli"
+let anotherusername="roli_rathour"
+if (userName==anotherusername) {
+    console.log("pick another userName")
+    
+}
+else {
+    console.log(" you can use this username")
+}
+// check whether a variable is number or not
+
+let score = 22
+if (typeof score=== 'number') {
+    console.log("yep, it is a number")
+    
+}else{
+    "Nope, It is not a number"
+}
+
+// boolean value is true or false
+let isTeaReady =true
+if (isTeaReady) {
+    console.log("tea is ready")
+} else {
+    console.log ("Tea is not ready")
+    
+}
+
+//to check array empty are not
+let Array=[]
+if (Array.length===0) {
+    console.log("Array is empty")
+} else {
+    console.log("Array is not empty")
+    
+}
