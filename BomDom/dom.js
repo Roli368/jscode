2403544388f87e3e39b86ee8e00cf54eaf29b336
Roli368 <rolirathour368@gmail.com>
2025-06-12 17:21:18 +0000

    dom bom

--- BomDom/dom.js
+++ BomDom/dom.js
@@ -12,8 +12,63 @@ document
             citieslist.firstElementChild.classList.add("highlight")
         })
 
-document.getElementById("ChangeOrder").addEventListener
-("click",function(){
+document
+.getElementById("ChangeOrder")
+.addEventListener("click",function(){
     let coffeeType=document.getElementById("coffeeType");
     coffeeType.textContent = "expresso";
   });
+
+  document
+  .getElementById("AddNewitems")
+  .addEventListener("click",function(){
+    let Newlist= document.createElement("li")
+    Newlist.textContent="Egg"
+    document.getElementById("ShoppingList").appendChild(Newlist)
+    
+   
+
+  })
+  document
+  .getElementById("RemoveItems")
+  .addEventListener("click",function(){
+    let tasklist= document.getElementById("TaskList") 
+    tasklist.lastElementChild.remove()
+  })
+
+  document
+  .getElementById("clickmebutton")
+  .addEventListener("mouseover",function(){
+    alert("I am roli")
+  })
+
+  // exercise 7
+  document
+  .getElementById("tealist")
+  .addEventListener("click",function(event){
+  if (event.target && event.target.matches(".teaitem")) {
+    alert("you have selected :"+ event.target.textContent)
+    
+  }
+  })
+  // exercise 8
+  document 
+  .getElementById("feedbackform").addEventListener("submit",function(event){
+    event.preventDefault();
+    let feedback= document.getElementById("feedbackinput").value;
+    console.log(feedback)
+    document.getElementById("feedbackdisplay").textContent=`feedback is: ${feedback}`
+  })
+
+  // exersise 9
+  document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("loading").textContent="Dom content fully loaded"
+  })
+
+  // exercise 10
+  document
+  .getElementById("togglehighlight")
+  .addEventListener("click",function(){
+    let descriptionText= document.getElementById("textdescription")
+    descriptionText.classList.toggle("highlight")
+  })
